12f97245885080a55c8ee9e1266661b22af33589
Roujwa <roujwa24si@mahasiswa.pcr.ac.id>
2025-10-14 11:39:28 +0700

    neww

---
+++ app/Models/Pelanggan.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Pelanggan extends Model
+{
+    protected $table        = 'pelanggan';
+    protected $primaryKey   = 'pelanggan.id';
+    protected $fillable     = [
+        'first_name',
+        'last_name',
+        'birthday',
+        'gender',
+        'email',
+        'phone',
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PelangganController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -50,5 +51,11 @@ Route::post('question/store', [QuestionController::class, 'store'])
         ->name('question.store');
 
 Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
+
+Route::post('/pelanggan/store', [PelangganController::class, 'store'])->name('pelanggan.store');
+
+Route::resource('pelanggan', PelangganController::class);
 
 
