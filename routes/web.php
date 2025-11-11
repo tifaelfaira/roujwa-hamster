d52f3a5981dcaedc2d63bcb89694d8ef04856d77
Roujwa <roujwa24si@mahasiswa.pcr.ac.id>
2025-11-11 11:00:04 +0700

    Perubahan Routes 2

--- routes/web.php
+++ routes/web.php
@@ -14,40 +14,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/pcr', function () {
-//     return 'Selamat Datang di Website Kampus PCR!';
-// });
-
-// Route::get('/mahasiswa', function () {
-//     return 'Halo Mahasiswa';
-// })->name('mahasiswa.show');
-
-// Route::get('/nama/{param1}', function ($param1) {
-//     return 'Nama saya: ' . $param1;
-// });
-
-// Route::get('/nim/{param1?}', function ($param1 = '') {
-//     return 'NIM saya: ' . $param1;
-// });
-
-// Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
-
-// Route::get('/about', function () {
-//     return view('halaman-about');
-// });
-
-// Route::get('/home', [HomeController::class, 'index'])
-//     ->name('home');
-
-// Route::get('/pegawai', [PegawaiController::class, 'index']);
-
-// Route::post('question/store', [QuestionController::class, 'store'])
-//     ->name('question.store');
-
-// Route::get('dashboard', [DashboardController::class, 'index'])
-//     ->name('dashboard');
-
-
 Route::resource('pelanggan', PelangganController::class);
 Route::resource('user', UserController::class);
 Route::resource('products', ProductController::class);
