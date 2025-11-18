0157f77a56f19484e4b5374b002fcb472e533c0f
Roujwa <roujwa24si@mahasiswa.pcr.ac.id>
2025-11-18 07:39:15 +0700

    pagination

--- database/seeders/CreatePelangganDummy.php
+++ database/seeders/CreatePelangganDummy.php
@@ -27,4 +27,3 @@ class CreatePelangganDummy extends Seeder
     }
 }
 }
-
--- resources/views/admin/pelanggan/index.blade.php
+++ resources/views/admin/pelanggan/index.blade.php
@@ -95,6 +95,9 @@
                                 @endforeach
                             </tbody>
                         </table>
+                        <div class="mt-3">
+                            {{ $dataPelanggan->links('pagination::bootstrap-5') }}
+                        </div>
                     </div>
                 </div>
             </div>
